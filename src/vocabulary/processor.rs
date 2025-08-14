af2074c902a2136891d8fea4fff86e3536071aa2
Alexandre <gourdel.alexandre@gmail.com>
2025-08-14 10:04:59 +0200

    remove debug

--- src/vocabulary/processor.rs
+++ src/vocabulary/processor.rs
@@ -156,13 +156,11 @@ impl TokenProcessor {
                     level: TokenProcessorLevel::Byte,
                 }),
                 DecoderWrapper::Sequence(decoding_sequence) => {
-                    println!("DecoderWrapper::Sequence");
                     let mut is_byte_fallback = false;
                     let mut is_byte_level = false;
                     let mut spacechar = ' '.to_string();
 
                     for decoder in decoding_sequence.get_decoders() {
-                        println!("decoder _ loop ");
                         match decoder {
                             DecoderWrapper::ByteFallback(_) => {
                                 is_byte_fallback = true;
